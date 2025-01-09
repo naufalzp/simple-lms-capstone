ede52be0b5fc6448764571664b856501aa909735
naufalzp <naufalarya0@gmail.com>
2025-01-09 19:44:13 +0700

    feat: add API routes for Course Categories and Course Feedback management
    with throttle

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,24 @@
 <?php
 
 use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\CourseCategoryController;
 use App\Http\Controllers\API\CommentController;
 use App\Http\Controllers\API\CourseAnnouncementController;
 use App\Http\Controllers\API\CourseContentController;
 use App\Http\Controllers\API\CourseController;
+use App\Http\Controllers\API\CourseFeedbackController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
+    Route::post('register', [AuthController::class, 'register'])->middleware('throttle:register');
 
     Route::middleware('auth:api')->group(function () {
         // Courses
         Route::get('my-courses', [CourseController::class, 'myCourses']);
         Route::get('courses', [CourseController::class, 'index']);
-        Route::post('courses', [CourseController::class, 'store']);
+        Route::post('courses', [CourseController::class, 'store'])->middleware('throttle:course_create');
         Route::get('courses/{id}', [CourseController::class, 'show']);
         Route::put('courses/{id}', [CourseController::class, 'update']);
         Route::delete('courses/{id}', [CourseController::class, 'destroy']);
@@ -25,8 +27,9 @@ Route::group(['prefix' => 'v1'], function () {
         Route::post('courses/{id}/enroll', [CourseController::class, 'enroll']);
         
         // Contents
+        Route::post('courses/{id}/contents', [CourseContentController::class, 'store'])->middleware('throttle:content_create');
         Route::get('contents/{id}/comments', [CourseContentController::class, 'listComments']);
-        Route::post('contents/{id}/comments', [CourseContentController::class, 'storeComment']);
+        Route::post('contents/{id}/comments', [CourseContentController::class, 'storeComment'])->middleware('throttle:comment');
         
         // Comments
         Route::delete('comments/{id}', [CommentController::class, 'destroy']);
@@ -36,6 +39,19 @@ Route::group(['prefix' => 'v1'], function () {
         Route::post('announcements', [CourseAnnouncementController::class, 'store']);
         Route::put('announcements/{id}', [CourseAnnouncementController::class, 'update']);
         Route::delete('announcements/{id}', [CourseAnnouncementController::class, 'destroy']);
+
+        // Categories
+        Route::get('categories', [CourseCategoryController::class, 'index']);
+        Route::post('categories', [CourseCategoryController::class, 'store']);
+        Route::get('categories/{id}', [CourseCategoryController::class, 'show']);
+        Route::delete('categories/{id}', [CourseCategoryController::class, 'destroy']);
+
+        // Feedback
+        Route::get('courses/{courseId}/feedbacks', [CourseFeedbackController::class, 'index']);
+        Route::post('courses/{courseId}/feedbacks', [CourseFeedbackController::class, 'store']);
+        Route::get('courses/{courseId}/feedbacks/{id}', [CourseFeedbackController::class, 'show']);
+        Route::put('courses/{courseId}/feedbacks/{id}', [CourseFeedbackController::class, 'update']);
+        Route::delete('courses/{courseId}feedbacks/{id}', [CourseFeedbackController::class, 'destroy']);
 
         // Auth
         Route::get('user', [AuthController::class, 'user']);
