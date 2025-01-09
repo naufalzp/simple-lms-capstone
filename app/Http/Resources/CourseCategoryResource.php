64a0a141bea91eb0fce6d6e0d25e6d5d91b40095
naufalzp <naufalarya0@gmail.com>
2025-01-09 14:31:56 +0700

    fix: update user_id to created_by in CourseCategoryResource and migration for consistency

--- app/Http/Resources/CourseCategoryResource.php
+++ app/Http/Resources/CourseCategoryResource.php
@@ -18,7 +18,7 @@ class CourseCategoryResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
-            'user_id' => $this->user_id,
+            'created_by' => $this->created_by,
             'user' => new UserResource($this->whenLoaded('user')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
