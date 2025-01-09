5be4036cbc062f17d2d0dfe5d305d111a880827b
naufalzp <naufalarya0@gmail.com>
2025-01-09 19:43:16 +0700

    feat: add feedbacks relationship to CourseMember model

--- app/Models/CourseMember.php
+++ app/Models/CourseMember.php
@@ -21,4 +21,9 @@ class CourseMember extends Model
     {
         return $this->belongsTo(Course::class);
     }
+
+    public function feedbacks()
+    {
+        return $this->hasMany(CourseFeedback::class, 'member_id');
+    }
 }
